83a1a181a19f6b9eb36cc06548453bc6e614d67b
hosam <hosamelmalt@gmail.com>
2020-03-22 11:03:28 +0200

    page is done

--- Doctoraak/View/Hosam/Custom Views/CustomSiftButton.swift
+++ Doctoraak/View/Hosam/Custom Views/CustomSiftButton.swift
@@ -21,11 +21,23 @@ class CustomSiftButton: UIButton {
         gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
         
         self.layer.insertSublayer(gradientLayer, at: 0)
-        
-//        layer.cornerRadius = rect.height / 2
-//        clipsToBounds = true
-        
         gradientLayer.frame = rect
     }
+}
+
+class CustomSiftSegmented: UISegmentedControl {
     
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        
+        let gradientLayer = CAGradientLayer()
+        let leftColor = #colorLiteral(red: 0.4747212529, green: 0.2048208416, blue: 1, alpha: 1)
+        let rightColor = #colorLiteral(red: 0.7187242508, green: 0.5294578671, blue: 0.9901599288, alpha: 1)
+        gradientLayer.colors = [leftColor.cgColor, rightColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0.5)
+        
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.frame = rect
+    }
 }
