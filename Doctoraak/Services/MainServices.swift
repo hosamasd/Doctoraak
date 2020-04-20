ab627a1cfa580c7a31fd4368fa3f58f188cfee4b
hosam <hosamelmalt@gmail.com>
2020-04-20 08:07:05 +0200

    register services is semi done

--- Doctoraak/Services/MainServices.swift
+++ Doctoraak/Services/MainServices.swift
@@ -12,24 +12,24 @@ class MainServices {
     static let shared = MainServices()
     
     func getDegrees(completion: @escaping (MainDegreeModel?, Error?) -> ())  {
-           let urlString = "https://doctoraak.sphinxat.us/public/api/show_degree"
-           MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-       }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_degree"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getSpecificationss(completion: @escaping (MainSpecificationModel?, Error?) -> ())  {
-              let urlString = "https://doctoraak.sphinxat.us/public/api/show_specialization"
-              MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-          }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_specialization"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getCitiess(completion: @escaping (MainCityModel?, Error?) -> ())  {
-                 let urlString = "https://doctoraak.sphinxat.us/public/api/show_city"
-                 MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-             }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_city"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getAreas(completion: @escaping (MainAreaModel?, Error?) -> ())  {
-                    let urlString = "https://doctoraak.sphinxat.us/public/api/show_area"
-                    MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-                }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_area"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getInsuracness(completion: @escaping (MainInsurcaneModel?, Error?) -> ())  {
         let urlString = "https://doctoraak.sphinxat.us/public/api/show_insurance"
@@ -37,40 +37,40 @@ class MainServices {
     }
     
     func getMedicineTypes(completion: @escaping (MainMedicineTypeModel?, Error?) -> ())  {
-           let urlString = "https://doctoraak.sphinxat.us/public/api/show_medicines_type"
-           MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-       }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_medicines_type"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getMedicines(completion: @escaping (MainMedicineModel?, Error?) -> ())  {
-              let urlString = "https://doctoraak.sphinxat.us/public/api/show_medicines"
-              MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-          }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_medicines"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getRays(completion: @escaping (MainRaysModel?, Error?) -> ())  {
-                 let urlString = "https://doctoraak.sphinxat.us/public/api/show_rays"
-                 MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-             }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_rays"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     func getNotifications(completion: @escaping (MainNotificationsModel?, Error?) -> ())  {
-                    let urlString = "https://doctoraak.sphinxat.us/public/api/show_specialization"
-                    MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
-                }
+        let urlString = "https://doctoraak.sphinxat.us/public/api/show_specialization"
+        MainServices.mainGetMethodGenerics(urlString: urlString, completion: completion)
+    }
     
     static func mainGetMethodGenerics<T:Codable>(urlString:String,completion:@escaping (T?,Error?)->Void)  {
-          
-          guard let url = URL(string: urlString) else { return }
-          URLSession.shared.dataTask(with: url) { (data, resp, err) in
-              if let err = err {
-                  completion(nil, err)
-                  return
-              }
-              do {
-                  let objects = try JSONDecoder().decode(T.self, from: data!)
-                  // success
-                  completion(objects, err)
-              } catch let error {
-                  completion(nil, error)
-              }
-              }.resume()
-      }
+        
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, resp, err) in
+            if let err = err {
+                completion(nil, err)
+                return
+            }
+            do {
+                let objects = try JSONDecoder().decode(T.self, from: data!)
+                // success
+                completion(objects, err)
+            } catch let error {
+                completion(nil, error)
+            }
+        }.resume()
+    }
 }
--- Doctoraak/Services/RegistrationServices.swift
+++ Doctoraak/Services/RegistrationServices.swift
@@ -14,70 +14,70 @@ class RegistrationServices {
     static let shared = RegistrationServices()
     
     func registerDoctor(isInsurance:Bool,coverImage:UIImage,cvName:String,cvFile:Data,name:String,email:String,phone:String,password:String,gender:String,specialization_id:Int,degree_id:Int,insurance:[Int] ,completion: @escaping (MainDoctorRegisterModel?, Error?) -> Void ) {
-            let urlString = "https://doctoraak.sphinxat.us/public/api/doctor_register"
-           
+        let urlString = "https://doctoraak.sphinxat.us/public/api/doctor_register"
+        
         let postString = urlString+"?name=\(name)&email=\(email)&phone=\(phone)&password=\(password)&gender=\(gender)&specialization_id=\(specialization_id)&degree_id=\(degree_id)"//insurance if not empty
-               
-               
-               Alamofire.upload(multipartFormData: { (multipartFormData) in
-                 
-                       if let data = coverImage.pngData() {
-                           multipartFormData.append(data, withName: "photo", fileName: "asd.jpeg", mimeType: "image/jpeg")
-                   }
+        
+        
+        Alamofire.upload(multipartFormData: { (multipartFormData) in
+            
+            if let data = coverImage.pngData() {
+                multipartFormData.append(data, withName: "photo", fileName: "asd.jpeg", mimeType: "image/jpeg")
+            }
+            
+            multipartFormData.append(cvFile, withName: "cv", fileName: cvName, mimeType:"application/pdf")
+        }, to:postString)
+        { (result) in
+            switch result {
+            case .success(let upload, _, _):
+                
+                upload.uploadProgress(closure: { (progress) in
+                    //Print progress
+                    print(progress)
+                })
+                
+                upload.responseJSON { response in
+                    //print response.result
+                    print(response.result)
+                    guard let data = response.data else {return}
                     
-                                       multipartFormData.append(cvFile, withName: "cv", fileName: cvName, mimeType:"application/pdf")
-                }, to:postString)
-               { (result) in
-                   switch result {
-                   case .success(let upload, _, _):
-                       
-                       upload.uploadProgress(closure: { (progress) in
-                           //Print progress
-                           print(progress)
-                       })
-                       
-                       upload.responseJSON { response in
-                           //print response.result
-                           print(response.result)
-                           guard let data = response.data else {return}
-                           
-                           do {
-                               let objects = try JSONDecoder().decode(MainDoctorRegisterModel.self, from: data)
-                               // success
-                               completion(objects,nil)
-                           } catch let error {
-                               completion(nil,error)
-                           }
-                           
-                       }
-                       
-                   case .failure( let encodingError):
-                       completion(nil,encodingError)
-                       break
-                       //print encodingError.description
-                   }
-               }
-         }
+                    do {
+                        let objects = try JSONDecoder().decode(MainDoctorRegisterModel.self, from: data)
+                        // success
+                        completion(objects,nil)
+                    } catch let error {
+                        completion(nil,error)
+                    }
+                    
+                }
+                
+            case .failure( let encodingError):
+                completion(nil,encodingError)
+                break
+                //print encodingError.description
+            }
+        }
+    }
     
-//    func loginUser(phone:String,password:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  {
-//           let urlString = "http://aqarzelo.com/public/api/user/login".toSecrueHttps()
-//           guard  let url = URL(string: urlString) else { return  }
-//           let postString = "phone=\(phone)&password=\(password)"
-//           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
-//       }
-//
-//       func forgetPassword(phone:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  { ////baseusermodel
-//           let urlString = "http://aqarzelo.com/public/api/user/forget-password".toSecrueHttps()
-//           guard  let url = URL(string: urlString) else { return  }
-//           let postString = "phone=\(phone)"
-//           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
-//       }
-//
-//       func receiveSmsCode(phone:String,code:String,api_Token:String,password:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  {
-//           let urlString = "http://aqarzelo.com/public/api/user/reset-password".toSecrueHttps()
-//           guard  let url = URL(string: urlString) else { return  }
-//
-//           let postString = "phone=\(phone)&password=\(password)&api_token=\(api_Token)&sms_code=\(code)"
-//           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
-//       }
+    //    func loginUser(phone:String,password:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  {
+    //           let urlString = "http://aqarzelo.com/public/api/user/login".toSecrueHttps()
+    //           guard  let url = URL(string: urlString) else { return  }
+    //           let postString = "phone=\(phone)&password=\(password)"
+    //           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
+    //       }
+    //
+    //       func forgetPassword(phone:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  { ////baseusermodel
+    //           let urlString = "http://aqarzelo.com/public/api/user/forget-password".toSecrueHttps()
+    //           guard  let url = URL(string: urlString) else { return  }
+    //           let postString = "phone=\(phone)"
+    //           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
+    //       }
+    //
+    //       func receiveSmsCode(phone:String,code:String,api_Token:String,password:String,completion:@escaping (BaseUserSecondModel?,Error?)->Void)  {
+    //           let urlString = "http://aqarzelo.com/public/api/user/reset-password".toSecrueHttps()
+    //           guard  let url = URL(string: urlString) else { return  }
+    //
+    //           let postString = "phone=\(phone)&password=\(password)&api_token=\(api_Token)&sms_code=\(code)"
+    //           RegistrationServices.registerationPostMethodGeneric(postString: postString, url: url, completion: completion)
+    //       }
 }
